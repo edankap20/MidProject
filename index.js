c4ae29948f07ef6fecc9d066678eaed708f66d44
edankap <edankap20@gmail.com>
2023-03-05 00:24:52 +0100

    Navar Done

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const nav = document.querySelector("#nav");
 const openMenu = document.querySelector("#menu-open");
 const closeMenu = document.querySelector("#menu-close");
-const navList = document.querySelector(".menu")
+const navList = document.querySelector(".menu");
+
+
+//creo un elemento nuevo li y a para "Contact Us"
+const newListItem = document.createElement("li");
+const newLink = document.createElement("a");
+newLink.textContent = "Contact Us";
+newLink.setAttribute("href", "/ContactPage/contactPage.html");
+// Agrega la clase al enlace
+newLink.classList.add("contact-us-link");
 
 
 openMenu.addEventListener("click", () => {
-    nav.classList.add("visible")
-//Verifica si ya existe el enlace "Contact Us" antes de agregar un nuevo
-    const contactUsLink = document.querySelector(".contact-us-link")
-
-// Si el enlace "contac Us" no existe, crea uno nuevo y agregalo a la lista
-    if(!contactUsLink) {
-        //creo un elemento nuevo li y a para "Contact Us"
-        const newListItem = document.createElement("li")
-        const newLink = document.createElement("a")
-        newLink.textContent = "Contact Us"
-        newLink.setAttribute("href", "/ContactPage/contactPage.html")
-        // Agrega la clase al enlace
-        newLink.classList.add("contact-us-link")
-
-        // Agrego el nuevo elemento a la lista de navegación
-        newListItem.appendChild(newLink)
-        navList.appendChild(newListItem)
-    }
-})
+    nav.classList.add("visible");
+    // Agrego el nuevo elemento a la lista de navegación
+    newListItem.appendChild(newLink);
+    navList.appendChild(newListItem);
+});
 
 closeMenu.addEventListener("click", () => {
-    nav.classList.remove("visible")
-})
+    nav.classList.remove("visible");
+
+    //Verifica si existe el enlace "Contact Us" en la lista y elimínalo
+    const contactUsLink = document.querySelector(".contact-us-link");
+    if (contactUsLink) {
+        const contactUsListItem = contactUsLink.closest("li");
+        navList.removeChild(contactUsListItem);
+    }
+});
