aa83187194d0e34c81c6d1dc42db7ced73fe0389
edankap <edankap20@gmail.com>
2023-03-13 23:06:53 +0100

    Desarrollo no terminado de JS para ProjectPage

--- ProjectPage/project_page.js
+++ ProjectPage/project_page.js
@@ -36,3 +36,39 @@ closeHamburguer.addEventListener("click", () => {
 
 });
 
+const SERVER_URL = 'https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects';
+
+window.onload = () => {
+    console.log('ONLOAD')
+    console.log(document.URL)
+    
+    //Para que me devuelva la query param que he agragado a la homepage en el enlace de Simplify ?leon=simplify
+    function _getProjectTitleOnURL() {
+        const params = new Proxy(new URLSearchParams(window.location.search), {
+            get: (searchParams, prop) => searchParams.get(prop),
+          });
+          return params.leon.toLowerCase();
+    }
+
+    //Para obtener los datos del servidor
+   async function _getProjectData () {
+      const response = await  fetch(SERVER_URL)
+      const projects = await response.json()
+      const [projectToShow] = projects.filter(project => project.name.toLowerCase() == _getProjectTitleOnURL())
+      console.log(projectToShow)
+
+      _updateProjectData(projectToShow)
+    }
+    // Actualiza los datos y los imprime en pantalla
+   function _updateProjectData(projectToShow) {
+        const projectTitleHeader = document.querySelector(".titleSimplify")
+        const proojectSubtitle = document.querySelector(".subtitle")
+        const projectData = document.querySelector(".simplifyDateSpam")
+        const paragraph = document.querySelector(".paragraphs")
+        const logoBlur = document.querySelector(".projectLogoBlur")
+        const logo = document.querySelector(".projectLogo")
+
+        projectTitleHeader.textContent = `${projectToShow.name} Johan`
+    };
+    _getProjectData;
+};
