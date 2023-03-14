39bf5d3e01865a7656c3df22cc9b1047e1a6c061
edankap <edankap20@gmail.com>
2023-03-14 20:41:16 +0100

    Project Page JS finished

--- ProjectPage/project_page.js
+++ ProjectPage/project_page.js
@@ -47,14 +47,16 @@ window.onload = () => {
         const params = new Proxy(new URLSearchParams(window.location.search), {
             get: (searchParams, prop) => searchParams.get(prop),
           });
-          return params.leon.toLowerCase();
+          console.log(params)
+          return params.title.toLowerCase();
     }
 
     //Para obtener los datos del servidor
    async function _getProjectData () {
       const response = await  fetch(SERVER_URL)
       const projects = await response.json()
-      const [projectToShow] = projects.filter(project => project.name.toLowerCase() == _getProjectTitleOnURL())
+      const projectTitle = _getProjectTitleOnURL()
+      const [projectToShow] = projects.filter(project => project.name.toLowerCase() === projectTitle)
       console.log(projectToShow)
 
       _updateProjectData(projectToShow)
@@ -62,13 +64,18 @@ window.onload = () => {
     // Actualiza los datos y los imprime en pantalla
    function _updateProjectData(projectToShow) {
         const projectTitleHeader = document.querySelector(".titleSimplify")
-        const proojectSubtitle = document.querySelector(".subtitle")
+        const projectSubtitle = document.querySelector(".subtitle")
         const projectData = document.querySelector(".simplifyDateSpam")
         const paragraph = document.querySelector(".paragraphs")
         const logoBlur = document.querySelector(".projectLogoBlur")
         const logo = document.querySelector(".projectLogo")
 
         projectTitleHeader.textContent = `${projectToShow.name} Johan`
+        projectSubtitle.textContent = projectToShow.description
+        projectData.textContent = projectToShow.completed_on
+        paragraph.textContent = projectToShow.content
+        logoBlur.src = projectToShow.image
+        logo.src = projectToShow.image
     };
-    _getProjectData;
+    _getProjectData();
 };
